1391298769390682ca2e73c0fa2e8773ec9dbbef
VelascoPatrik <patrik.messelberger@whiteduck.de>
2024-02-11 19:00:43 +0100

    Sidebar + burger animation + Links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
+import Sidebar from "../sidebar/Sidebar"
 import "./navbar.scss"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         {/*Sidebar*/}
+        <Sidebar/>
         <div className="wrapper">
             <span>Messelberger Patrik</span>
             <div className="social">
